92be06fcac18ddc9612b116fa46c1b812a900c91
Davidodole <124046210+Davidodole@users.noreply.github.com>
2024-01-19 14:46:40 -0800

    creating some changes

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 const button = document.querySelector("button").addEventListener("click", ()=>{
     document.querySelector("nav ul").classList.toggle("open");
 });
-window.addEventListener("load",()=>{
-    document.querySelector(".preload").style.display = "none";
-});
