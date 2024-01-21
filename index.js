ca894b977181914fc3e1e648c8b9860b4b412865
Davidodole <124046210+Davidodole@users.noreply.github.com>
2024-01-21 06:33:26 -0800

    THis is my first react project to be upload to github

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const button = document.querySelector("button").addEventListener("click", ()=>{
     document.querySelector("nav ul").classList.toggle("open");
+    const body = document.body;
+    body.style.position = "fixed";
 });
+// creating a text changer in the home setion 
+var typed = new Typed("#textAnimation", {
+    strings: ["UI/UX Developer","Frontend Developer", "Backend Developer"],
+    typeSpeed: 150,
+    smartBackspace: true,
+    backSpeed: 25,
+
+  });
